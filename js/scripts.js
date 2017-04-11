c3ca40dd81622f6e472f57fbc32919ee20e7635b
christophszcz <christoph.szczechowicz@gmail.com>
2017-04-11 17:49:11 -0400

    Add jQuery to be able to access bootstrap.min.js.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var array = [];
+/* Operations */
+var addition = document.getElementById('addition');
+var one = document.getElementById('one');
+var addition = document.getElementById('addition');
+var equals = document.getElementById('equals');
+var clear = document.getElementById('clear');
+
+one.addEventListener('keydown', function(){
+	array.push(1);
+});
