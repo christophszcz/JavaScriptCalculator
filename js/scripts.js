9138c4ea5f06c4394412379fa60d8b50e45f6848
christophszcz <christoph.szczechowicz@gmail.com>
2017-04-12 11:05:30 -0400

    Create subtraction algorithm.

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,6 @@ var one = document.getElementById('one');
 var two = document.getElementById('two');
 var three = document.getElementById('three');
 
-/* Operations */
-var addition = document.getElementById('addition');
-var addition = document.getElementById('addition');
-var equals = document.getElementById('equals');
-var clear = document.getElementById('clear');
-
 one.addEventListener('click', function(){
 	array.push(1);
 });
@@ -24,11 +18,27 @@ three.addEventListener('click', function(){
 	array.push(3);
 });
 
+/* Operations */
+var addition = document.getElementById('addition');
+var subtraction = document.getElementById('subtraction');
+var equals = document.getElementById('equals');
+var clear = document.getElementById('clear');
+
+
 addition.addEventListener('click', function(){
 	sum = array.reduce(add, 0);  
 	function add(a, b) {     
 		return a + b; 
 	}  
+});
+
+subtraction.addEventListener('click', function(){
+	while(array.length > 1){
+		var i =0, j=1;
+		var difference = array[i] - array[j];
+		array.splice(0,2);
+		array.unshift(difference);
+	}
 });
 
 clear.addEventListener('click', function(){
