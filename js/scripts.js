2bfa89b6293ad09318fe2dbdc80406b01de944c6
christophszcz <christoph.szczechowicz@gmail.com>
2017-04-12 11:33:44 -0400

    Expand the addition algorithm to multiple digits.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var array = [];
+var tempArray = [], array = [];
 
 /* Numbers */
 
@@ -7,15 +7,15 @@ var two = document.getElementById('two');
 var three = document.getElementById('three');
 
 one.addEventListener('click', function(){
-	array.push(1);
+	tempArray.push(1);
 });
 
 two.addEventListener('click', function(){
-	array.push(2);
+	tempArray.push(2);
 });
 
 three.addEventListener('click', function(){
-	array.push(3);
+	tempArray.push(3);
 });
 
 /* Operations */
@@ -25,16 +25,16 @@ var equals = document.getElementById('equals');
 var clear = document.getElementById('clear');
 
 
-addition.addEventListener('click', function(){
-	sum = array.reduce(add, 0);  
-	function add(a, b) {     
-		return a + b; 
-	}  
+addition.addEventListener('click', function additionFunction(){
+	var multipleDigits = tempArray.join('');
+	var trueNum = parseInt(multipleDigits);
+	array.push(trueNum);
+	tempArray = [];
 });
 
 subtraction.addEventListener('click', function(){
 	while(array.length > 1){
-		var i =0, j=1;
+		var i = 0, j = 1;
 		var difference = array[i] - array[j];
 		array.splice(0,2);
 		array.unshift(difference);
@@ -43,4 +43,19 @@ subtraction.addEventListener('click', function(){
 
 clear.addEventListener('click', function(){
 	array = [];
+});
+
+equals.addEventListener('click', function (){
+	var multipleDigits = tempArray.join('');
+	var trueNum = parseInt(multipleDigits);
+	array.push(trueNum);
+	tempArray = [];
+
+	if (array.length > 1){
+		sum = array.reduce(add, 0);  
+		function add(a, b) {     
+			return a + b; 
+		}  
+		console.log(sum);
+	}
 });
