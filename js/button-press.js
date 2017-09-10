bb1df180a8cdfe7363463ee9911e69075825351f
christophszcz <christoph.szczechowicz@gmail.com>
2017-09-09 23:19:14 -0400

    Remove the focus after the button has been clicked.

--- js/button-press.js
+++ js/button-press.js
@@ -6,3 +6,11 @@ window.addEventListener('keydown', function(e){
 	tempArray.push(e.key);
 	display.value = tempArray.join('');
 });
+
+function removeTransition(e){
+	if (e.propertyName !== 'focus') return;
+	this.classList.remove('focus');
+}
+
+const buttons = document.querySelectorAll('.btn'); 
+buttons.forEach(button => button.addEventListener('transitionend', removeTransition));
