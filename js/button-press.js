d13cfe442cf531db24bba6ac7db856aad7c41b82
christophszcz <christoph.szczechowicz@gmail.com>
2017-09-12 09:45:52 -0400

    Remove the button keyboard button press focus class.

--- js/button-press.js
+++ js/button-press.js
@@ -1,19 +1,14 @@
 window.addEventListener('keydown', function(e){
-	const button = document.querySelector(`.btn[data-key="${e.keyCode}"`);
+	const button = document.querySelector(`.btn[data-key="${e.keyCode}"]`);
 	if (!button) return;
-	button.classList.toggle('focus');
+	button.classList.add('focus');
 	if (isNaN(e.key) === false){
 		tempArray.push(Number(e.key));
 		display.value = tempArray.join('');
 	}else{
 		decimalActions();
-	} 
+	}
+	setTimeout(function(){
+		button.classList.remove('focus');
+	},500);
 });
-
-function removeTransition(e){
-	if (e.propertyName !== 'focus') return;
-	this.classList.remove('focus');
-}
-
-const buttons = document.querySelectorAll('.btn'); 
-buttons.forEach(button => button.addEventListener('transitionend', removeTransition));
